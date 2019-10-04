5ca641e8807d63d4cb45a6e564c1760caf46ecfe
ernestomedina17 <ernestomedina17@gmail.com>
2019-10-04 17:27:22 -0500

    Fixing ingress config

---
+++ files/set_apache_html_index.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export POD0=`kubectl get pods -l run=apache-web-server -o jsonpath='{range .items[0]}{@.metadata.name}{"\n"}'`
+export POD1=`kubectl get pods -l run=apache-web-server -o jsonpath='{range .items[1]}{@.metadata.name}{"\n"}'`
+kubectl exec -i $POD0 -- sh -c 'echo pod_A > /usr/local/apache2/htdocs/index.html' 
+kubectl exec -i $POD1 -- sh -c 'echo pod_B > /usr/local/apache2/htdocs/index.html' 
