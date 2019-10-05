568cd04ec0c1d6801a1080fc339a298a3d87f5a7
ernestomedina17 <ernestomedina17@gmail.com>
2019-10-05 14:52:38 -0500

    Adding SSL annotations to ingress

--- files/set_apache_html_index.sh
+++ files/set_apache_html_index.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 180
 export POD0=`kubectl get pods -l run=apache-web-server -o jsonpath='{range .items[0]}{@.metadata.name}{"\n"}'`
 export POD1=`kubectl get pods -l run=apache-web-server -o jsonpath='{range .items[1]}{@.metadata.name}{"\n"}'`
 kubectl exec -i $POD0 -- sh -c 'echo pod_A > /usr/local/apache2/htdocs/index.html' 
